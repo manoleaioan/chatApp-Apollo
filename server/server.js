127f9f7d62b8fff7882c2949b0a7ead4e153c9ed
manolea ioan <ioanmanolea@gmail.com>
2021-06-05 22:15:03 +0300

    up

--- server/server.js
+++ server/server.js
@@ -28,10 +28,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 if (process.env.NODE_ENV == 'production'){
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 
   app.get('*', function(req, res){
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
 }
 
